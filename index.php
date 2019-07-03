a1cd4daf6aa874a407bdefb91ec6f002ccb2c002
Rafael Audax <rafael@grupoaudax.com.br>
2019-07-03 19:32:18 +0000

    Added handle Sabo.

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ set_time_limit(0);
 
 require_once __DIR__. '/bootstrap.php';
 
-define('FILE_NAME', 'sabo-site-code');
-define('TYPE_CRAWLER', 'sabo-site-code');
+define('FILE_NAME', 'sabo');
+define('TYPE_CRAWLER', 'sabo');
 
 App::init();

ba1cb48cbce95d84a08f9c10a6a6983ecad2b61d
Rafael Dimas <rafael@grupoaudax.com.br>
2019-09-13 15:43:23 -0300

    Define placeholder value in constants FILE_NAME and TYPE_CRAWLER.

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ set_time_limit(0);
 
 require_once __DIR__. '/bootstrap.php';
 
-define('FILE_NAME', 'google');
-define('TYPE_CRAWLER', 'google');
+define('FILE_NAME', 'REPLACE_WITH_FILE_NAME');
+define('TYPE_CRAWLER', 'REPLACE_WITH_HANDLER');
 
 App::init();
